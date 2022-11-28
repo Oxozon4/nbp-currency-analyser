0ab1ceda7c4ee37875ecd6182ad11e87ebf280d0
Oxozon <oxozon.o4@gmail.com>
2022-11-28 20:11:57 +0100

    task 5 | add comment 1

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 print("Semafor. Oxozon4 - scrum master, developer")
 
-
+# comment 1
 class IArithmeticsAdd:
     def Addition(self, A:float, B:float)->float:
         pass
@@ -12,7 +12,7 @@ class IArithmeticsDiff:
 class IArithmeticsMult:
     def Multiplication(self, A: float, B: float) -> float:
         pass
- 
+
 class IArithmeticsDiv:
     def Division(self, A: float, B: float) -> float:
         pass
