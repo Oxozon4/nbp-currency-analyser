f93d0af8cd71c41aadc100b8a9ef4e13b288d00d
p-olszewski <przemo1297@gmail.com>
2022-11-28 20:17:48 +0100

    TASK5 Add second comment

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 print("Semafor. Oxozon4 - scrum master, developer")
+
+
+class IArithmeticsAdd:
+    def Addition(self, A:float, B:float)->float:
+        pass
+#comment1
+class IArithmeticsDiff:
+    def Difference(self, A: float, B: float) -> float:
+        pass
+#comment2
+class IArithmeticsMult:
+    def Multiplication(self, A: float, B: float) -> float:
+        pass
+
+class IArithmeticsDiv:
+    def Division(self, A: float, B: float) -> float:
+        pass
