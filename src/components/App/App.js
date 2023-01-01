a0881ebbae196d5a9e5a5b7f109af087cc7ee5eb
Oxozon <oxozon.o4@gmail.com>
2023-01-01 17:23:40 +0100

    change components folder structure, remove reportWebVitals

---
+++ src/components/App/App.js
@@ -0,0 +1,66 @@
+import { useState, useEffect } from 'react';
+import Loader from '../Loader/Loader';
+import { ToastContainer } from 'react-toastify';
+import './App.scss';
+import 'react-toastify/dist/ReactToastify.css';
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [currency] = useState('usd');
+  const [days] = useState(7);
+  const [currencyValue, setCurrencyValue] = useState(0);
+  const [apiResponseData, setApiResponseData] = useState(null);
+
+  const url = `//api.nbp.pl/api/exchangerates/rates/a/${currency}/last/${days}`;
+
+  // onload
+  useEffect(() => {
+    const getCurrencyData = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+      setApiResponseData(data);
+      setCurrencyValue(data.rates[days - 1].mid);
+      setIsLoading(false);
+    };
+    setIsLoading(true);
+    getCurrencyData();
+  }, [days, url]);
+
+  useEffect(() => {
+    if (apiResponseData) {
+      console.log(apiResponseData);
+    }
+  }, [apiResponseData]);
+
+  return (
+    <>
+      <Loader isLoading={isLoading} />
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <div className="app">
+        <a
+          className="app-link"
+          href="http://api.nbp.pl/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          API NBP
+        </a>
+        <div>
+          Uwaga! Aktualny kurs {currency}: {currencyValue} zł
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default App;
