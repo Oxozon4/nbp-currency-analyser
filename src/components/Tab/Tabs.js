7e3179bc82e4653e3073bdb525d84e2ea5ce5d3b
p-olszewski <przemo1297@gmail.com>
2023-01-05 21:24:15 +0100

    Move api request from App.js to Tabs.js component

--- src/components/Tab/Tabs.js
+++ src/components/Tab/Tabs.js
@@ -1,9 +1,33 @@
-import { useState , useEffect} from 'react';
-import './Tabs.scss';
+import { useState, useEffect } from "react";
+import "./Tabs.scss";
 
-const Tabs = () => {
+const Tabs = ({ setIsLoading }) => {
   const [tabIndex, setTabIndex] = useState(0);
   const [timeInterval, setTimeInterval] = useState(7);
+  const [currency] = useState("USD");
+  const [days] = useState(7);
+  const [currencyValue, setCurrencyValue] = useState(0);
+  const [apiResponseData, setApiResponseData] = useState(null);
+  const url = `//api.nbp.pl/api/exchangerates/rates/a/${currency}/last/${days}`;
+
+  // onload
+  useEffect(() => {
+    const getCurrencyData = async () => {
+      const response = await fetch(url);
+      const data = await response.json();
+      setApiResponseData(data);
+      setCurrencyValue(data.rates[days - 1].mid);
+      setIsLoading(false);
+    };
+    setIsLoading(true);
+    getCurrencyData();
+  }, [days, setIsLoading, url]);
+
+  useEffect(() => {
+    if (apiResponseData) {
+      console.log(apiResponseData);
+    }
+  }, [apiResponseData]);
 
   useEffect(() => {
     console.log(timeInterval);
@@ -14,7 +38,7 @@ const Tabs = () => {
       <div className="tabs-list">
         <div
           className={`tabs-list-header ${
-            tabIndex === 0 ? 'tabs-list-header_active' : ''
+            tabIndex === 0 ? "tabs-list-header_active" : ""
           }`}
           onClick={() => setTabIndex(0)}
         >
@@ -22,7 +46,7 @@ const Tabs = () => {
         </div>
         <div
           className={`tabs-list-header ${
-            tabIndex === 1 ? 'tabs-list-header_active' : ''
+            tabIndex === 1 ? "tabs-list-header_active" : ""
           }`}
           onClick={() => setTabIndex(1)}
         >
@@ -32,26 +56,33 @@ const Tabs = () => {
       <div
         data-testid="tab-content1"
         className={`tabs-content ${
-          tabIndex === 0 ? 'tabs-content_active' : ''
+          tabIndex === 0 ? "tabs-content_active" : ""
         }`}
       >
-        Tab 1 content1
         <div>
-      <label htmlFor="time-interval-selector">Wybierz przedział czasowy:</label>
-      <select id="time-interval-selector" onChange={e => setTimeInterval(e.target.value)}>
-        <option value="7">1 tydzień</option>
-        <option value="14">2 tygodnie</option>
-        <option value="30">1 miesiąc</option>
-        <option value="90">1 kwartał</option>
-        <option value="180">pół roku</option>
-        <option value="365">1 rok</option>
-      </select>
-    </div>
+          Aktualny kurs {currency}: {currencyValue} zł
+        </div>
+        <div>
+          <label htmlFor="time-interval-selector">
+            Wybierz przedział czasowy:
+          </label>
+          <select
+            id="time-interval-selector"
+            onChange={(e) => setTimeInterval(e.target.value)}
+          >
+            <option value="7">1 tydzień</option>
+            <option value="14">2 tygodnie</option>
+            <option value="30">1 miesiąc</option>
+            <option value="90">1 kwartał</option>
+            <option value="180">pół roku</option>
+            <option value="365">1 rok</option>
+          </select>
+        </div>
       </div>
       <div
         data-testid="tab-content2"
         className={`tabs-content ${
-          tabIndex === 1 ? 'tabs-content_active' : ''
+          tabIndex === 1 ? "tabs-content_active" : ""
         }`}
       >
         Tab 2 content
