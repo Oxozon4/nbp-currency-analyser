d21b0db59c9271312396206f26b51e0b4b46d4b8
p-olszewski <przemo1297@gmail.com>
2023-01-06 19:29:33 +0100

    Fix tab1 components order

--- src/components/Tab/Tabs.js
+++ src/components/Tab/Tabs.js
@@ -1,7 +1,7 @@
-import { useState, useEffect } from 'react';
-import { toast } from 'react-toastify';
-import SelectCurrency from '../SelectCurrency/SelectCurrency';
-import './Tabs.scss';
+import { useState, useEffect } from "react";
+import { toast } from "react-toastify";
+import SelectCurrency from "../SelectCurrency/SelectCurrency";
+import "./Tabs.scss";
 
 const Tabs = ({ setIsLoading }) => {
   const [tabIndex, setTabIndex] = useState(0);
@@ -9,19 +9,19 @@ const Tabs = ({ setIsLoading }) => {
   const [currencyCode] = useState("USD");
   const [currencyValue, setCurrencyValue] = useState(0);
   const [apiResponseData, setApiResponseData] = useState(null);
-  const [selectedCurrency, setSelectedCurrency] = useState('USD');
+  const [selectedCurrency, setSelectedCurrency] = useState("USD");
 
   const getUrl = () => {
     const today = new Date();
     const todayDateString = `${today.getFullYear()}-${String(
       today.getMonth() + 1
-    ).padStart(2, '0')}-${String(today.getDate()).padStart(2, '0')}`;
+    ).padStart(2, "0")}-${String(today.getDate()).padStart(2, "0")}`;
     const previousDate = new Date(
       today.setDate(today.getDate() - timeInterval)
     );
     const previousDateString = `${previousDate.getFullYear()}-${String(
       previousDate.getMonth() + 1
-    ).padStart(2, '0')}-${String(previousDate.getDate()).padStart(2, '0')}`;
+    ).padStart(2, "0")}-${String(previousDate.getDate()).padStart(2, "0")}`;
     const url = `//api.nbp.pl/api/exchangerates/rates/a/${selectedCurrency}/${previousDateString}/${todayDateString}/`;
     return url;
   };
@@ -31,13 +31,13 @@ const Tabs = ({ setIsLoading }) => {
     const response = await fetch(url);
     if (!response.ok) {
       toast.error(
-        'Wystąpił problem przy pobieraniu danych! Spróbuj ponownie później!'
+        "Wystąpił problem przy pobieraniu danych! Spróbuj ponownie później!"
       );
     }
     const data = await response.json();
     setApiResponseData(data);
     setCurrencyValue(data.rates[data.rates.length - 1].mid.toFixed(2));
-    toast.success('Dane pobrane pomyślnie!', { id: 'test' });
+    toast.success("Dane pobrane pomyślnie!", { id: "test" });
     setIsLoading(false);
   };
 
@@ -62,7 +62,7 @@ const Tabs = ({ setIsLoading }) => {
       <div className="tabs-list">
         <div
           className={`tabs-list-header ${
-            tabIndex === 0 ? 'tabs-list-header_active' : ''
+            tabIndex === 0 ? "tabs-list-header_active" : ""
           }`}
           onClick={() => setTabIndex(0)}
         >
@@ -70,7 +70,7 @@ const Tabs = ({ setIsLoading }) => {
         </div>
         <div
           className={`tabs-list-header ${
-            tabIndex === 1 ? 'tabs-list-header_active' : ''
+            tabIndex === 1 ? "tabs-list-header_active" : ""
           }`}
           onClick={() => setTabIndex(1)}
         >
@@ -80,7 +80,7 @@ const Tabs = ({ setIsLoading }) => {
       <div
         data-testid="tab-content1"
         className={`tabs-content ${
-          tabIndex === 0 ? 'tabs-content_active' : ''
+          tabIndex === 0 ? "tabs-content_active" : ""
         }`}
       >
         <div>
@@ -102,62 +102,6 @@ const Tabs = ({ setIsLoading }) => {
             <option value="365">1 rok</option>
           </select>
         </div>
-        tabela wyników
-  <table>
-    <tr>
-      <td>/</td>
-      <td>Mediana</td>
-      <td>Dominata</td>
-      <td>Odchylenie standardowe</td>
-      <td>Współczynnik zmienności</td>
-    </tr>
-    <tr>
-      <td>1 tydzień</td>
-      <td>Column 2, Row 2</td>
-      <td>Column 3, Row 2</td>
-      <td>Column 4, Row 2</td>
-      <td>Column 5, Row 2</td>
-    </tr>
-    <tr>
-      <td>2 tygodnie</td>
-      <td>Column 2, Row 3</td>
-      <td>Column 3, Row 3</td>
-      <td>Column 4, Row 3</td>
-      <td>Column 5, Row 3</td>
-    </tr>
-    <tr>
-      <td>1 miesiąc</td>
-      <td>Column 2, Row 4</td>
-      <td>Column 3, Row 4</td>
-      <td>Column 4, Row 4</td>
-      <td>Column 5, Row 4</td>
-    </tr>
-    <tr>
-      <td>1 kwartał</td>
-      <td>Column 2, Row 5</td>
-      <td>Column 3, Row 5</td>
-      <td>Column 4, Row 5</td>
-      <td>Column 5, Row 5</td>
-    </tr>
-    <tr>
-      <td>Pół roku</td>
-      <td>Column 2, Row 6</td>
-      <td>Column 3, Row 6</td>
-      <td>Column 4, Row 6</td>
-      <td>Column 5, Row 6</td>
-    </tr>
-    <tr>
-      <td>Rok</td>
-      <td>Column 2, Row 7</td>
-      <td>Column 3, Row 7</td>
-      <td>Column 4, Row 7</td>
-      <td>Column 5, Row 7</td>
-      
-    </tr>
-  </table>
-
-
-        {/* Tab 1 content */}
         <SelectCurrency
           name="select-currency"
           value={selectedCurrency}
@@ -169,11 +113,63 @@ const Tabs = ({ setIsLoading }) => {
         >
           Oblicz
         </button>
+        tabela wyników
+        <table>
+          <tr>
+            <td>/</td>
+            <td>Mediana</td>
+            <td>Dominata</td>
+            <td>Odchylenie standardowe</td>
+            <td>Współczynnik zmienności</td>
+          </tr>
+          <tr>
+            <td>1 tydzień</td>
+            <td>Column 2, Row 2</td>
+            <td>Column 3, Row 2</td>
+            <td>Column 4, Row 2</td>
+            <td>Column 5, Row 2</td>
+          </tr>
+          <tr>
+            <td>2 tygodnie</td>
+            <td>Column 2, Row 3</td>
+            <td>Column 3, Row 3</td>
+            <td>Column 4, Row 3</td>
+            <td>Column 5, Row 3</td>
+          </tr>
+          <tr>
+            <td>1 miesiąc</td>
+            <td>Column 2, Row 4</td>
+            <td>Column 3, Row 4</td>
+            <td>Column 4, Row 4</td>
+            <td>Column 5, Row 4</td>
+          </tr>
+          <tr>
+            <td>1 kwartał</td>
+            <td>Column 2, Row 5</td>
+            <td>Column 3, Row 5</td>
+            <td>Column 4, Row 5</td>
+            <td>Column 5, Row 5</td>
+          </tr>
+          <tr>
+            <td>Pół roku</td>
+            <td>Column 2, Row 6</td>
+            <td>Column 3, Row 6</td>
+            <td>Column 4, Row 6</td>
+            <td>Column 5, Row 6</td>
+          </tr>
+          <tr>
+            <td>Rok</td>
+            <td>Column 2, Row 7</td>
+            <td>Column 3, Row 7</td>
+            <td>Column 4, Row 7</td>
+            <td>Column 5, Row 7</td>
+          </tr>
+        </table>
       </div>
       <div
         data-testid="tab-content2"
         className={`tabs-content ${
-          tabIndex === 1 ? 'tabs-content_active' : ''
+          tabIndex === 1 ? "tabs-content_active" : ""
         }`}
       >
         Tab 2 content
