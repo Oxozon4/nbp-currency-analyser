0e2cd9f3b81be4bab06034efb42162162842cda4
p-olszewski <przemo1297@gmail.com>
2023-01-14 11:28:18 +0100

    Add header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <div className="header-container">
+      <span>SEMAFOR-APP</span>
+    </div>
+  );
+};
+
+export default Header;
