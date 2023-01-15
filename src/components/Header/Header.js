c532ba42c30626b5463203542fddfbff529b7b58
Oxozon <oxozon.o4@gmail.com>
2023-01-15 08:40:02 +0100

    add refresh action for header button

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './Header.scss';
 
 const Header = () => {
+  const onClickHandler = () => {
+    const event = new Event('refreshAction');
+    document.dispatchEvent(event);
+  };
+
   return (
     <div className="header-container">
       <span>SEMAFOR-APP</span>
-      <button onClick={null}>Odśwież</button>
+      <button onClick={onClickHandler}>Odśwież</button>
     </div>
   );
 };
